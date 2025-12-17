0645e0d78a4b5d987d2aeb973b6782f4a8c8a820
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2025-12-16 19:17:51 -0600

    changed from jsdom to happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     // setupFiles: ['src/setupTests.ts'],
     include: [
         'src/__tests__/**/*.{test,spec}.{ts,tsx}'
     ],
-    exclude: ['node_modules', 'dist']
+    exclude: ['node_modules', 'dist'],
+    restoreMocks: true
   }
 })
